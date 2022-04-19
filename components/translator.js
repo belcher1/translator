b3cd50b17e47b4cd0943c5d228cdc4d45c2dc655
Andrew Belcher <belcher.w.andrew@gmail.com>
2022-04-19 17:41:44 +0800

    commit Times & Titles

--- components/translator.js
+++ components/translator.js
@@ -3,9 +3,102 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+
 class Translator {
 
-    
+    time(text, locale) {
+
+        let spanStart = '<span class="highlight">';
+        let spanEnd = '</span>';        
+
+        let timeRegex = /[0-2]*[0-9][:\.][0-5][0-9]/g;
+
+        let translatedTime = text;
+
+        if(timeRegex.test(text) && locale == 'american-to-british') {
+
+            let matches = text.match(timeRegex);
+            let newTimes =[]; 
+
+            // console.log(matches);
+            // console.log(matches.length);
+
+            for(let i = 0; i < matches.length; i++) {
+                newTimes.push(matches[i].replace(':', '.'));
+                
+                translatedTime = translatedTime.replace(matches[i], spanStart + newTimes[i] + spanEnd);
+                // console.log(translatedTime);
+            }
+        }
+
+        if(timeRegex.test(text) && locale == 'british-to-american') {
+
+            let matches = text.match(timeRegex);
+            let newTimes =[]; 
+
+            // console.log(matches);
+            // console.log(matches.length);
+
+            for(let i = 0; i < matches.length; i++) {
+                newTimes.push(matches[i].replace('.', ':'));
+                
+                translatedTime = translatedTime.replace(matches[i], spanStart + newTimes[i] + spanEnd);
+                // console.log(translatedTime);
+            }
+        }
+
+        return translatedTime;
+    }
+
+    titles(text, locale) {
+
+        // console.log(text);   
+
+        // let obj = americanToBritishTitles;
+
+        let spanStart = '<span class="highlight">';
+        let spanEnd = '</span>'; 
+
+        let americanTitles = Object.keys(americanToBritishTitles);
+        let britishTitles = Object.values(americanToBritishTitles);
+
+        let translatedText = text;
+
+        if(locale == 'american-to-british') {
+            americanTitles.forEach((title) => {
+                // translatedText = translatedText.replace(new RegExp('\^' + title + '\\s', 'gi'), spanStart + americanToBritishTitles[title] + ' ' + spanEnd);
+                // translatedText = translatedText.replace(new RegExp('\\s' + title + '\\s', 'gi'), spanStart + ' ' + americanToBritishTitles[title] + ' ' + spanEnd);
+                translatedText = translatedText.replace(new RegExp(title, 'gi'), spanStart + americanToBritishTitles[title] + spanEnd);
+            });
+        }
+
+        if(locale == 'british-to-american') {
+            britishTitles.forEach((title, i) => {
+                // translatedText = translatedText.replace(new RegExp('\^' + title + '\\s', 'gi'), spanStart + americanTitles[i] + ' ' + spanEnd);
+                // translatedText = translatedText.replace(new RegExp('\\s' + title + '\\s', 'gi'), spanStart + ' ' + americanTitles[i] + ' ' + spanEnd);
+                translatedText = translatedText.replace(new RegExp(title, 'gi'), spanStart + americanTitles[i] + spanEnd);
+            });
+        }
+
+        // console.log(translatedText);
+        return translatedText;
+    }
+
+    translate(text, locale) {
+        let spanStart = '<span class="highlight">';
+        let spanEnd = '</span>'; 
+
+        if(locale == 'american-to-british') {
+
+        }
+
+        if(locale == 'british-to-american') {
+            
+        }
+
+        // console.log(translatedText);
+        return translatedText;
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,41 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
 
-      
+      // console.log(req.body);
+
+      let text = req.body.text;
+      let locale = req.body.locale;
+
+      let translation = '';
+
+      //#6 Check if one or more fields are missing 
+      if(locale == undefined || text == undefined) {
+        return res.json({error: 'Required field(s) missing'});
+      }
+
+      //#7 Check if text is empty
+      if(text == '') {
+        return res.json({error: 'No text to translate'}); 
+      }
+
+      //#8 Check if locale matches one of the two specified
+      let option1 = 'american-to-british';
+      let option2 = 'british-to-american';
+      if(locale != option1 && locale != option2) {
+        return res.json({error: 'Invalid value for locale field'});
+      }
+
+      translation = translator.time(text, locale);
+      translation = translator.titles(translation, locale);
+      translation = translator.translate(translation, locale);
+
+      //#9 Check if the text required no translation
+      if(translation == text) {
+        // console.log('test');
+        return res.json({text: text, translation: 'Everything looks good to me!'});
+      }
+
+      return res.json({text: text, translation: translation});
     });
 };
